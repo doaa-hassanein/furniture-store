4a67dd08cbe15cd22c35bd93acc7d5f7801ec241
Doaa Hassanein <doaahassanein418@gmail.com>
2026-08-27 20:31:20 +0300

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   eslint: {
+    ignoreDuringBuilds: true, // تجاهل أخطاء ESLint أثناء البناء
+  },
 };
 
 export default nextConfig;
